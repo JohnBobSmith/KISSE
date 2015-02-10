0fc7130e8bdd7f49b3d661dddd3362ebb6c3c143
JohnBobSmith <myrunescapeemail609@gmail.com>
2015-02-09 19:45:38 -0700

    Added example program

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+/*
+    Keep It Stupid Simple Engine
 
-using namespace std;
+    Written by JohnBobSmith and
+    muttstuffle, as a hobby project.
+
+    Notice:
+    You are free to use this code
+    in any way you wish, provided
+    you give credit to the original
+    authors.
+*/
+
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    cout << "Hello world!"<<"?" << endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML TEST");
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Green);
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
 }
